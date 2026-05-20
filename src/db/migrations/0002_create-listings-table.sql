219c282ca4b7fe2536710d8222ac7116e907d02a
fifa-s <svoboda.fili@gmail.com>
2026-05-20 12:10:18 +0200

    create listings db table

---
+++ src/db/migrations/0002_create-listings-table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS `listings` (
+  `id` INTEGER PRIMARY KEY AUTOINCREMENT,
+  `itemName` TEXT NOT NULL,
+  `itemDescription` TEXT,
+  `itemCategory` TEXT NOT NULL,
+  `itemPrice` INTEGER NOT NULL,
+  `contactName` TEXT,
+  `contactEmail` TEXT,
+  `listingState` TEXT NOT NULL
+);
---
+++ src/db/schemas/listings.schema.ts
@@ -0,0 +1,14 @@
+import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+
+export const listings = sqliteTable("listings", {
+  id: integer().primaryKey({ autoIncrement: true }),
+  itemName: text().notNull(),
+  itemDescription: text(),
+  itemCategory: text().notNull(),
+  itemPrice: integer().notNull(),
+  contactName: text(),
+  contactEmail: text(),
+  listingState: text().notNull(),
+});
+
+export type Listings = typeof listings.$inferSelect;
