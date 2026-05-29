dfe14dbb61178b2ed86df46025e2b15eec1225ee
fifa-s <svoboda.fili@gmail.com>
2026-05-29 11:34:10 +0200

    Added language dorp down and chinese

--- src/types/locale.ts
+++ src/types/locale.ts
@@ -1,8 +1,9 @@
 export const LOCALE_CODE = {
   cs: "cs",
   en: "en",
+  zh: "zh",
 } as const;
 
 export type LocaleCode = (typeof LOCALE_CODE)[keyof typeof LOCALE_CODE];
 
-export const LOCALE_CODES: readonly LocaleCode[] = ["cs", "en"];
+export const LOCALE_CODES: readonly LocaleCode[] = ["cs", "en", "zh"];
