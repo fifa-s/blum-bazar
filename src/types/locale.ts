0852115a31c867ac1343a2223cc5cd785146554c
fifa-s <svoboda.fili@gmail.com>
2026-05-29 11:14:04 +0200

    Add english locale

--- src/types/locale.ts
+++ src/types/locale.ts
@@ -1,7 +1,8 @@
 export const LOCALE_CODE = {
   cs: "cs",
+  en: "en",
 } as const;
 
 export type LocaleCode = (typeof LOCALE_CODE)[keyof typeof LOCALE_CODE];
 
-export const LOCALE_CODES: readonly LocaleCode[] = ["cs"];
+export const LOCALE_CODES: readonly LocaleCode[] = ["cs", "en"];
